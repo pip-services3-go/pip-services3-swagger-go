1ad3b98fd967f25041fea6034af882065b688c07
Dmitry Krayniy <krdima92@gmail.com>
2022-08-15 11:05:13 +0400

    Updated pipeline

--- resgen.ps1
+++ resgen.ps1
@@ -6,35 +6,34 @@ $ErrorActionPreference = "Stop"
 # Generate image and container names using the data in the "component.json" file
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
 
-$resImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-res"
-$container=$component.name
+$resImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)-res"
+$container = $component.name
 
 # Remove build files
-if (Test-Path "./resources") {
-    Remove-Item -Recurse -Force -Path "./resources/*"
+if (Test-Path "$PSScriptRoot/resources") {
+    Remove-Item -Recurse -Force -Path "$PSScriptRoot/resources/*"
 } else {
-    $null = New-Item -ItemType Directory -Force -Path "./resources"
+    $null = New-Item -ItemType Directory -Force -Path "$PSScriptRoot/resources"
 }
-if (Test-Path "./example/resources") {
-    Remove-Item -Recurse -Force -Path "./example/resources/*"
+if (Test-Path "$PSScriptRoot/example/resources") {
+    Remove-Item -Recurse -Force -Path "$PSScriptRoot/example/resources/*"
 } else {
-    $null = New-Item -ItemType Directory -Force -Path "./example/resources"
+    $null = New-Item -ItemType Directory -Force -Path "$PSScriptRoot/example/resources"
 }
 
 # Build docker image
-docker build -f docker/Dockerfile.resgen -t $resImage .
+docker build -f "$PSScriptRoot/docker/Dockerfile.resgen" -t $resImage "$PSScriptRoot/."
 
 # Run resgen container
 docker run -d --name $container $resImage
 
 # Copy resources from container
-docker cp "$($container):/app/src/example/resources" ./example
-docker cp "$($container):/app/src/resources" .
+docker cp "$($container):/app/src/example/resources" "$PSScriptRoot/example"
+docker cp "$($container):/app/src/resources" "$PSScriptRoot/."
 # Remove docgen container
 docker rm $container --force
 
 # Verify resources
-if (!(Test-Path "./resources/*.go")) {
-    Write-Host "resources folder doesn't exist in root dir. Watch logs above."
-    exit 1
+if (-not (Test-Path "$PSScriptRoot/resources/*.go")) {
+    Write-Error "resources folder doesn't exist in root dir. Watch logs above."
 }
